a3e5782d4853326fe035c6cb4acb81cd6e6dea2b
AhsanM1470 <ahsanmahfuz04@gmail.com>
2023-10-16 13:26:33 +0100

    Add a sign up form

--- microblogs/views.py
+++ microblogs/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 # from django.http import HttpResponse
 
 def home(request):
     # response = HttpResponse("Welcome to Clucker")
     return render(request, "home.html")
+
+def sign_up(request):
+    # response = HttpResponse("Welcome to Clucker")
+    form = SignUpForm()
+    return render(request, "sign_up.html", {'form': form})
